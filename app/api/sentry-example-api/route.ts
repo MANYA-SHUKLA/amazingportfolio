97865f27b57b52f7f0f1ea6f2bf27f43ca632bd7
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2026-02-01 03:30:29 +0530

    Implement custom error throwing in GET function
    Throw a custom error in the GET function for Sentry testing.

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-
 export const dynamic = "force-dynamic";
 class SentryExampleAPIError extends Error {
   constructor(message: string | undefined) {
@@ -7,7 +6,6 @@ class SentryExampleAPIError extends Error {
     this.name = "SentryExampleAPIError";
   }
 }
-
 export function GET() {
   throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
   return NextResponse.json({ data: "Testing Sentry Error..." });
