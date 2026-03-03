ea8a59cf59fe5dfeeefe2ef9bf023cc9cd423e0e
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2026-03-03 16:01:04 +0530

    Remove unnecessary blank lines in Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { workExperience } from "@/data";
 import { Button } from "./ui/MovingBorders";
-
 const Experience = () => {
   return (
     <div className="py-20 w-full" id="experience">
